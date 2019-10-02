46925b12cb07fdd99ed21add0d6d40b1abd1badc
Christoph Trautwein <christoph.trautwein@sinnerschrader.com>
2019-10-02 17:20:33 +0000

    Updates

---
+++ docs/precache-manifest.a0212e158f5ce48d0f38e32e94a67512.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77e62571482ea332456b96a7d0ffff7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "020a68893e20510ead39",
+    "url": "./static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "1db84ed400d60a40f338",
+    "url": "./static/js/2.dc94816f.chunk.js"
+  },
+  {
+    "revision": "020a68893e20510ead39",
+    "url": "./static/js/main.a79855f9.chunk.js"
+  },
+  {
+    "revision": "90149d904717343c875a",
+    "url": "./static/js/runtime-main.be8fd0a8.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
